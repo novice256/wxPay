2e1540921df6a32af1634e1a691ab611903ee99b
novice256 <1040891012@qq.com>
2017-08-18 09:40:04 +0800

    update

--- wxPay.php
+++ wxPay.php
@@ -49,4 +49,65 @@ class wxPay{
         $payPackage['sign'] = $paySign;
         echo json_encode($payPackage);
     }
+
+     /**
+     * 微信退款
+     * @param $order_id
+     * @return bool
+     * @throws \Exception
+     */
+    protected function wxRefund($order_id) {
+        $order = M('appOrder')->where("id=%d", $order_id)->field('id,order_no,ali_transaction_no,amount')->find();
+
+        /************************************************************/
+        include_once "WxPay.pub.config.php";
+        require_once "UnifiedOrderPub.php";
+
+        // 获取prepay_id
+        $unifiedOrder = new \UnifiedOrderPub();
+
+        // 计算paySign
+        $payPackage = [
+            'appid' => \WxPayConfPub::APPID,
+            'mch_id' => \WxPayConfPub::MCHID,
+            'nonce_str' => $unifiedOrder->createNoncestr(),
+            'transaction_id'=> $order['ali_transaction_no'],//微信订单号 1.2二选一,商户侧传给微信的订单号
+            'out_refund_no' => $order['order_no'],//商户内部唯一退款单号
+            'total_fee'=> intval($order['amount'] * 100),//总金额
+            'refund_fee'=> intval($order['amount'] * 100)//退款金额
+        ];
+        $paySign = $unifiedOrder->getSign($payPackage);
+        $payPackage['sign'] = $paySign;
+        $url = "https://api.mch.weixin.qq.com/secapi/pay/refund";//微信退款地址，post请求
+        $xml = $unifiedOrder->arrayToXml($payPackage);
+
+        //退款
+        $data = $unifiedOrder->refund($xml,$url);
+
+        if($data){
+            $result = $unifiedOrder->xmlToArray($data);
+            if(($result['return_code']=='SUCCESS') && ($result['result_code']=='SUCCESS')){
+                //退款成功
+                $rs['status'] = 1;
+                $rs['msg'] = '退款成功';
+                return $rs;
+            }else if(($result['return_code']=='FAIL') || ($result['result_code']=='FAIL')){
+                //退款失败
+                //原因
+                $reason = (empty($result['err_code_des'])?$result['return_msg']:$result['err_code_des']);
+                $rs['status'] = 0;
+                $rs['msg'] = $reason;
+                return $rs;
+            }else{
+                //失败
+                $rs['status'] = 0;
+                $rs['msg'] = '退款成功';
+                return $rs;
+            }
+        }else{
+            $rs['status'] = 0;
+            $rs['msg'] = '退款成功';
+            return $rs;
+        }
+    }
 }
